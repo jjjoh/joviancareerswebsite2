67c7fbad7b47f3b3fc3bccec32548f2cc04e49b6
Johanne Schatz <johanne.schatz@avanade.com>
2024-01-31 14:51:44 +0100

    connect securely to DB & print jobs from the DB

---
+++ database.py
@@ -0,0 +1,38 @@
+
+
+from sqlalchemy import create_engine, text
+import os
+import mysql.connector
+from dotenv import load_dotenv
+load_dotenv()
+
+connection = mysql.connector.connect(
+  host= os.getenv("DB_HOST"),
+  user=os.getenv("DB_USERNAME"),
+  password= os.getenv("DB_PASSWORD"),
+  database= os.getenv("DB_NAME"),
+  ssl_verify_identity=True,
+  ssl_ca='C:/ssl/cacert-2023-12-12.pem'
+)
+
+
+connection_string = str(connection) 
+
+url = "https://example.com?host=" + os.getenv("DB_HOST") + "&user=" + os.getenv("DB_USERNAME") + "&password=" + os.getenv("DB_PASSWORD") + "&database=" + os.getenv("DB_NAME") + "&autocommit=True&ssl_mode=VERIFY_IDENTITY&ssl_ca=/etc/ssl/cert.pem"
+
+
+engine = create_engine(
+    "mysql+pymysql://2sjk5ktin3xwi79i79qx:pscale_pw_GG6Y6FXzMhp1uQ2LWkahK7S2lRQWuiegWECX5uXh57x@aws.connect.psdb.cloud/joviancareers?charset=utf8mb4",
+    connect_args={
+        "ssl":{
+            "ssl_verify_identity":True,
+            "ca" : "C:/ssl/cacert-2023-12-12.pem"
+        }
+    }
+    )
+
+with engine.connect() as conn:
+    result = conn.execute(text("select * from jobs"))
+    print(result.all())
+
+    
--- flaskapp.py
+++ flaskapp.py
@@ -1,42 +1,28 @@
 from flask import Flask, render_template, jsonify
+from database import engine
+from sqlalchemy import text
 
 app = Flask(__name__)
 
-JOBS = [
-    {
-        'id': 1,
-        'title': 'Data Analyst',
-        'location':'Bengaluru, India',
-        'salary': 'Rs. 10,00,000'
-    },
-     {
-        'id': 2,
-        'title': 'Data Scientist',
-        'location':'Delhi, India',
-        'salary': 'Rs. 15,00,000'
-    },
-     {
-        'id': 3,
-        'title': 'Frontend Engineer',
-        'location':'Remote',
-        'salary': 'Rs. 12,00,000'
-    },
-      {
-        'id': 4,
-        'title': 'Backend Engineer',
-        'location':'Remote'
-    }
-]
+def load_jobs_from_db():
+    with engine.connect() as conn:
+        result = conn.execute(text("select * from jobs"))
+        jobs = []
+        for row in result.all():
+            jobs.append(dict(row))
+        return jobs        
 
 @app.route("/")
-def hello_world():
+def hello_jovian():
+    jobs = load_jobs_from_db()
     return render_template('home.html' ,
-                           jobs=JOBS,
+                           jobs=jobs,
                            company_name='Jovian')
 
 @app.route("/api/jobs")
 def list_jobs():
-    return jsonify(JOBS)
+    return jsonify(load_jobs_from_db)
+
 
 if __name__ == '__main__':
     app.run(host= '0.0.0.0', debug=True)
