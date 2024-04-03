e41f68c9fd67e10ba2c75c70312562215d555aeb
Johanne Schatz <johanne.schatz@avanade.com>
2024-04-03 14:16:42 +0200

    add timezone, prometheus-client in dockerfile & datetime, metrics.info, modified /api/jobs route in flaskapp.py

--- dockerfile
+++ dockerfile
@@ -3,6 +3,9 @@
 # Utilise une image de base Python built for the linux/amd64 platform (render.com requirement)
 FROM --platform=linux/amd64 python:3.12-alpine
 
+# Set the timezone to Europe/Paris
+ENV TZ=Europe/Paris
+
 #copy the content of my current directory in my local system to a new directory named app in the docker image file system
 COPY . /app
 
@@ -14,7 +17,7 @@ WORKDIR /app
 
 # Install necessary dependencies
 RUN apk update && \
-    apk add --no-cache gcc musl-dev mariadb-connector-c-dev pkgconf
+    apk add --no-cache gcc musl-dev mariadb-connector-c-dev pkgconf prometheus-client
 
 # Install Python dependencies from requirements.txt
 RUN pip install -r requirements.txt
--- flaskapp.py
+++ flaskapp.py
@@ -1,10 +1,14 @@
 from flask import Flask, render_template, jsonify
 from database import load_jobs_from_db
 from prometheus_flask_exporter import PrometheusMetrics
+import datetime
 
 
 app = Flask(__name__)
-metrics = PrometheusMetrics(app)     
+metrics = PrometheusMetrics(app)
+
+# Métriques par défaut pour les requêtes HTTP
+metrics.info('app_info', 'Application info', version='1.0.3')
 
 @app.route("/")
 def hello_jovian():
@@ -15,11 +19,15 @@ def hello_jovian():
 
 @app.route("/api/jobs")
 def list_jobs():
-    return jsonify(load_jobs_from_db)
+    return jsonify(jobs)
 
 @app.route('/always_ok', methods=['GET'])
 def always_ok():
     return jsonify({'message': 'OK'}), 200
+
+@app.route("/time")
+def time():
+    return str(datetime.now())
 
 if __name__ == '__main__':
     app.run(host= '0.0.0.0', port=5000, debug=True)
